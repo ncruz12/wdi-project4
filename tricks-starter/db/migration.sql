2e27fba1842af72ee3f3df083a6b5bd361a8b8bb
nayeli cruz <nayelicruz12@gmail.com>
2017-11-03 16:15:26 -0400

    changed db

--- tricks-starter/db/migration.sql
+++ tricks-starter/db/migration.sql
@@ -1,15 +1,32 @@
-\c css_tricks_db
+\c css_tricks_db;
 
-DROP TABLE IF EXISTS css_types;
+-- DROP TABLE IF EXISTS css_types;
+-- DROP TABLE IF EXISTS tricks;
+
+-- CREATE TABLE css_types (
+--   id SERIAL PRIMARY KEY,
+--   css_type VARCHAR(75)
+-- );
+
+-- CREATE TABLE tricks (
+--   id SERIAL PRIMARY KEY,
+--   description VARCHAR(150),
+--   css_type_id INTEGER REFERENCES css_types(id)
+-- );
+
 DROP TABLE IF EXISTS tricks;
+DROP TABLE IF EXISTS codes;
 
-CREATE TABLE css_types (
+
+CREATE TABLE codes (
   id SERIAL PRIMARY KEY,
-  css_type VARCHAR(75)
+  html_code TEXT,
+  css_code TEXT
 );
 
 CREATE TABLE tricks (
   id SERIAL PRIMARY KEY,
-  description VARCHAR(150),
-  css_type_id INTEGER REFERENCES css_types(id)
+  category VARCHAR(75),
+  description VARCHAR(250),
+  codes_id INTEGER REFERENCES codes(id)
 );
--- tricks-starter/db/seed.sql
+++ tricks-starter/db/seed.sql
@@ -1,34 +1,180 @@
 \c css_tricks_db;
 
-INSERT INTO css_types (css_type) VALUES
-('Gradients'),
-('Images'),
-('Animations'),
-('Transitions'),
-('2D Transforms'),
-('3D Transforms');
+-- INSERT INTO css_types (css_type) VALUES
+-- ('Gradients'),
+-- ('Images'),
+-- ('Animations'),
+-- ('Transitions'),
+-- ('2D Transforms'),
+-- ('3D Transforms');
+
+INSERT INTO tricks (category, description, codes_id) VALUES
+('Gradients', 'CSS3 gradients allows you to display transitions betweent two or more colors and can be in a different shape.', 1),
+('Images', 'There are many things that can be done with images, but for this example I will show how to flip an image.',2),
+('Animations', 'CSS3 animations allow an element to change from one style to another', 3),
+('Transition', 'CSS3 transitions allow you to change property values from one value to another, over a given time', 4),
+('Opacity', 'This property determines the transperancy of a element', 5),
+('Shadow', 'CSS3 allows you to add shadow to text or elements, for this example we will be doing it on text with multiple colors', 6);
 
 
-INSERT INTO tricks (description, css_type_id) VALUES
-('CSS3 gradients allows you to display transitions betweent two or more colors.',
-  1
-),
-('CSS3 gradients allows you to display transitions betweent two or more colors and can be in a different shape.',
-  1
-),
-('Applying CSS with images ',
-  2
-),
-('CSS3 animations allow an element to change from one style to another',
-  3
-),
-('CSS3 transitions allow you to change property values from one value to another, over a given time',
-  4
-),
-('CSS3 animations allow an element to change from one style to another and its pretty cool what you can do with it!',
-  3
-),
-('This is called the matrix method. it combines all the D2 transform methods into one ',
-  5
-);
+INSERT INTO codes (html_code, css_code) VALUES
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Gradient</title>
+</head>
+<body>
+  <h1>This is a Radial Gradient as a Circle</h1>
+  <div id="gradient"></div>
+</body>
+</html>
+  ',
+  '
+  #gradient {
+  height: 170px;
+  width: 210px;
+  background: radial-gradient(circle, purple, lightpink, lightblue);
+}
+  '),
+
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Images</title>
+</head>
+<body>
+  <div id="images">
+    <h1>Hover over image to view effect</h1>
+    <img src="" alt="picture">
+  </div>
+</body>
+</html>
+  ',
+  '
+  images img:hover {
+    transform: scaleX(-1);
+  }
+  '),
+
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Animations</title>
+</head>
+<body>
+  <h1>This animation changes the color and shape of the element</h1>
+  <div class="animation"></div>
+</body>
+</html>
+  ',
+  '
+  .animation {
+  height: 150px;
+  width: 150px;
+  margin: 0 auto;
+  background-color: blue;
+  animation-name: stretch;
+  animation-duration: 2s;
+  animation-timing-function: ease-out;
+  animation-direction: alternate;
+  animation-iteration-count: infinite;
+}
+
+  @keyframes stretch {
+  0% {
+    transform: scale(.3);
+    background-color: blue;
+    border-radius: 100%;
+  }
+  50% {
+    background-color: purple;
+  }
+  100% {
+    transform: scale(1.5);
+    background-color: red;
+  }
+}
+  '),
+
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Transitions</title>
+</head>
+<body>
+  <div class="transition">
+    <h1>Hover over me</h1>
+  </div>
+</body>
+</html>
+  ',
+  '
+  .transition {
+  width: 150px;
+  height: 300px;
+  background-color: lightblue;
+  transition: all 1s ease-in-out;
+}
+
+  .transition:hover {
+  width: 350px;
+  height: 150px;
+  background-color: lightgreen;
+}
+'),
+
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Opacity</title>
+</head>
+<body>
+  <div class="opacity">
+    <img src="" alt="image">
+  </div>
+</body>
+</html>
+  ',
+  '
+  .opacity img {
+  opacity: 0.5;
+  }
+
+  .opacity img:hover {
+  opacity: 1.0;
+  }
+  '),
+
+('
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Text Shadow</title>
+</head>
+<body>
+  <div class="shadow">
+    <h1>This is a cool effect !</h1>
+  </div>
+</body>
+</html>
+  ',
+  '
+  .shadow h1 {
+  color:  white;
+  text-shadow: 1px 1px 2px black, 0 0 30px blue, 0 0 5px darkblue;
+  }
+  ');
+
+
 
