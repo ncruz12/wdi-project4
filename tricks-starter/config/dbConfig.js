4cb760c4324819c43449925560e059b604820e8a
nayeli cruz <nayelicruz12@gmail.com>
2017-11-06 15:57:10 -0500

    changed file so pg-promise works w/ heroku

--- tricks-starter/config/dbConfig.js
+++ tricks-starter/config/dbConfig.js
@@ -1,5 +1,23 @@
-module.exports = process.env.DATABASE_URL || {
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'css_tricks_db',
-    port: process.env.DB_PORT || 5432,
-  };
+// module.exports = process.env.DATABASE_URL || {
+//     host: process.env.DB_HOST || 'localhost',
+//     database: process.env.DB_NAME || 'css_tricks_db',
+//     port: process.env.DB_PORT || 5432,
+//   };
+
+
+const pgp = require('pg-promise')();
+
+let db;
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'css_tricks_db',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
+}
+
+module.exports = db;
+
