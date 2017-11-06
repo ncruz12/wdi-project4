fa2af0a66d53348f2503f3599fcdf9d4a909ebe7
nayeli cruz <nayelicruz12@gmail.com>
2017-11-06 14:08:39 -0500

    removed edit,add, and delete

--- tricks-starter/controllers/tricksController.js
+++ tricks-starter/controllers/tricksController.js
@@ -1,16 +1,16 @@
 const trick = require('../models/trick');
 
 module.exports = {
-  makeBlankTrick(req, res, next) {
-    const blankTrick = {
-      id: null,
-      category: null,
-      description: null,
-      codes_type: null,
-    };
-    res.locals.trick = blankTrick;
-    next();
-  },
+  // makeBlankTrick(req, res, next) {
+  //   const blankTrick = {
+  //     id: null,
+  //     category: null,
+  //     description: null,
+  //     codes_type: null,
+  //   };
+  //   res.locals.trick = blankTrick;
+  //   next();
+  // },
   index(req, res, next) {
     trick.findAll()
       .then((tricks) => {
@@ -27,32 +27,32 @@ module.exports = {
     })
     .catch(err => next(err));
   },
-  create(req, res, next) {
-    trick.save(req.body)
-      .then((trick) => {
-        res.locals.trick = trick;
-        next();
-      })
-      .catch(err => next(err));
-  },
-  update(req, res, next) {
-    console.log(req.body, 'update controller');
-    trick.update(req.body)
-      .then((trick) => {
-        res.locals.trick = trick;
-        next();
-      })
-      .catch(err => next(err));
-  },
-  destroy(req, res, next) {
-    trick.destroy(req.params.id)
-      .then(() => next())
-      .catch(err => next(err));
-  },
-  showTrickForm: (req, res) => {
-    res.json({
-      message: 'I’m the HTML form for new tricks. I post to /tricks',
-    });
-  },
+  // create(req, res, next) {
+  //   trick.save(req.body)
+  //     .then((trick) => {
+  //       res.locals.trick = trick;
+  //       next();
+  //     })
+  //     .catch(err => next(err));
+  // },
+  // update(req, res, next) {
+  //   console.log(req.body, 'update controller');
+  //   trick.update(req.body)
+  //     .then((trick) => {
+  //       res.locals.trick = trick;
+  //       next();
+  //     })
+  //     .catch(err => next(err));
+  // },
+  // destroy(req, res, next) {
+  //   trick.destroy(req.params.id)
+  //     .then(() => next())
+  //     .catch(err => next(err));
+  // },
+  // showTrickForm: (req, res) => {
+  //   res.json({
+  //     message: 'I’m the HTML form for new tricks. I post to /tricks',
+  //   });
+  // },
 
 };
--- tricks-starter/controllers/viewController.js
+++ tricks-starter/controllers/viewController.js
@@ -2,10 +2,10 @@ module.exports = {
   show404(err, req, res, next) {
     res.sendStatus(404);
   },
-  show406(err, req, res, next) {
-    console.log('in error function');
-    res.sendStatus(406);
-  },
+  // show406(err, req, res, next) {
+  //   console.log('in error function');
+  //   res.sendStatus(406);
+  // },
   showTricks(req, res) {
     res.render('tricks/trick-index', {
       data: res.locals.tricks,
@@ -17,22 +17,22 @@ module.exports = {
       // console.log(res.locals.trick);
     });console.log(res.locals.trick);
   },
-  showAddForm(req, res) {
-    res.render('tricks/trick-add');
-  },
-  showEditForm(req, res) {
-    res.render('tricks/trick-edit', {
-      data: res.locals.trick,
-    });
-  },
-  handleCreate(req, res) {
-    res.redirect('/tricks');
-  },
-  handleUpdate(req, res) {
-    // console.log('in handle update');
-    res.redirect(`/tricks/${req.params.id}`);
-  },
-  handleDelete(req, res) {
-    res.redirect('/tricks');
-  },
+  // showAddForm(req, res) {
+  //   res.render('tricks/trick-add');
+  // },
+  // showEditForm(req, res) {
+  //   res.render('tricks/trick-edit', {
+  //     data: res.locals.trick,
+  //   });
+  // },
+  // handleCreate(req, res) {
+  //   res.redirect('/tricks');
+  // },
+  // handleUpdate(req, res) {
+  //   // console.log('in handle update');
+  //   res.redirect(`/tricks/${req.params.id}`);
+  // },
+  // handleDelete(req, res) {
+  //   res.redirect('/tricks');
+  // },
 };
