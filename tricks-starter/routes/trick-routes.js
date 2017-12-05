346848a33f8ab41f14c4a33fa76d0099662be222
nayeli cruz <nayelicruz12@gmail.com>
2017-12-05 16:34:42 -0500

    change routes to adjust to info

--- tricks-starter/routes/trick-routes.js
+++ tricks-starter/routes/trick-routes.js
@@ -4,17 +4,17 @@ const views = require('../controllers/viewController');
 
 const tricksRoutes = express.Router();
 
-tricksRoutes.get('/:id/edit', controller.getOne, views.showEditForm, views.show404);
+// tricksRoutes.get('/:id/edit', controller.getOne, views.showEditForm, views.show404);
 
-tricksRoutes.get('/new', views.showAddForm);
+// tricksRoutes.get('/new', views.showAddForm);
 
 tricksRoutes.route('/:id')
-  .get(controller.getOne, views.showOne, views.show404)
-  .put(controller.update, views.handleUpdate, views.show406)
-  .delete(controller.destroy, views.handleDelete, views.show404);
+  .get(controller.getOne, views.showOne, views.show404);
+  // .put(controller.update, views.handleUpdate, views.show406)
+  // .delete(controller.destroy, views.handleDelete, views.show404);
 
 tricksRoutes.route('/')
-  .get(controller.index, views.showTricks, views.show404)
-  .post(controller.create, views.handleCreate, views.show406);
+  .get(controller.index, views.showTricks, views.show404);
+  // .post(controller.create, views.handleCreate, views.show406);
 
   module.exports = tricksRoutes;
